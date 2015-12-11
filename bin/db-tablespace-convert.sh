eaa3f70b0925e216787bc3f7fc34911925035e73
Raphael Huefner <raphael@affinitybridge.com>
2015-12-11 15:42:58 -0800

    allow MyISAM --> InnoDB coercion

--- bin/db-tablespace-convert.sh
+++ bin/db-tablespace-convert.sh
@@ -10,11 +10,31 @@ if [ $LAST_RETURN_VALUE -ne 0 ]; then
 fi
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 DBNAME"
-  exit
+  echo "Usage: $0 [--force-innodb] DBNAME"
+  echo "--force-innodb : force all MyISAM tables to be converted into InnoDB"
+  exit 2
 fi
 
-DBNAME=$1
+force_innodb=""
+
+if [ '--force-innodb' == "$1" -a -n "$2" ]; then
+  DBNAME=$2
+  force_innodb=TRUE
+else
+  DBNAME=$1
+  if [ '--force-innodb' == "$2" ]; then
+    force_innodb=TRUE
+  fi
+fi
+
+if [ -n "$force_innodb" ]; then
+  echo "Forcing MyISAM tables to be converted into InnoDB."
+  select_tables_engine_clause="ENGINE LIKE 'InnoDB' OR ENGINE LIKE 'MyISAM'"
+  alter_table_engine_clause='ENGINE=InnoDB'
+else
+  select_tables_engine_clause="ENGINE LIKE 'InnoDB'"
+  alter_table_engine_clause=''
+fi
 
 db-tablespace-create-if-not-exists.sh $DBNAME
 LAST_RETURN_VALUE=$?
@@ -26,7 +46,7 @@ fi
 tablespacename="`db-tablespace-get-name.sh $DBNAME`"
 
 echo "Start converting DB '$DBNAME' into separate general tablespace '$tablespacename'."
-mysql --login-path=$DB_LOGIN_PATH_ADMIN -e"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA LIKE '$DBNAME' AND ENGINE LIKE 'InnoDB';" --skip-column-names "$DBNAME" | while read tablename; do
+mysql --login-path=$DB_LOGIN_PATH_ADMIN -e"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA LIKE '$DBNAME' AND ($select_tables_engine_clause);" --skip-column-names "$DBNAME" | while read tablename; do
   echo -n "TABLE '$DBNAME.$tablename': "
   backtick='`'
   table_definition_with_tablespace="`mysql --login-path=$DB_LOGIN_PATH_ADMIN -e"SHOW CREATE TABLE $DBNAME.$tablename;" --silent --skip-column-names | grep -E "TABLESPACE $backtick$tablespacename$backtick"`"
@@ -34,7 +54,7 @@ mysql --login-path=$DB_LOGIN_PATH_ADMIN -e"SELECT TABLE_NAME FROM INFORMATION_SC
     echo "Already in tablespace '$tablespacename'."
   else
     echo -n "Converting into tablespace '$tablespacename'. "
-    mysql --login-path=$DB_LOGIN_PATH_ADMIN -e"ALTER TABLE $DBNAME.$tablename TABLESPACE $tablespacename;" 
+    mysql --login-path=$DB_LOGIN_PATH_ADMIN -e"ALTER TABLE $DBNAME.$tablename $alter_table_engine_clause TABLESPACE $tablespacename;"
     LAST_RETURN_VALUE=$?
     if [ $LAST_RETURN_VALUE -eq 0 ]; then
       echo "Done."
--- bin/db-tablespace-import.sh
+++ bin/db-tablespace-import.sh
@@ -10,11 +10,29 @@ if [ $LAST_RETURN_VALUE -ne 0 ]; then
 fi
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 DBNAME"
-  exit
+  echo "Usage: $0 [--force-innodb] DBNAME"
+  echo "--force-innodb : force all MyISAM tables to be imported as InnoDB"
+  exit 2
 fi
 
-DBNAME=$1
+force_innodb=""
+
+if [ '--force-innodb' == "$1" -a -n "$2" ]; then
+  DBNAME=$2
+  force_innodb=TRUE
+else
+  DBNAME=$1
+  if [ '--force-innodb' == "$2" ]; then
+    force_innodb=TRUE
+  fi
+fi
+
+if [ -n "$force_innodb" ]; then
+  echo "Forcing MyISAM tables to be imported as InnoDB instead."
+  engine='(MyISAM|InnoDB)'
+else
+  engine='InnoDB'
+fi
 
 db-tablespace-create-if-not-exists.sh $DBNAME
 LAST_RETURN_VALUE=$?
@@ -26,4 +44,4 @@ fi
 tablespacename="`db-tablespace-get-name.sh $DBNAME`"
 
 # Why use LC_ALL? ---> @see http://stackoverflow.com/questions/19242275/re-error-illegal-byte-sequence-on-mac-os-x
-LC_ALL=C sed "s/ENGINE=InnoDB/TABLESPACE $tablespacename ENGINE=InnoDB/g" < /dev/stdin | mysql --login-path=$DB_LOGIN_PATH_NORMAL "$DBNAME"
+LC_ALL=C sed -E "s/ENGINE=$engine/TABLESPACE $tablespacename ENGINE=InnoDB/g" < /dev/stdin | mysql --login-path=$DB_LOGIN_PATH_NORMAL "$DBNAME"
