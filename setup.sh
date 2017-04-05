c1febd26676a67f8d6c5c9f9a5902c944a3cb516
Raphael Huefner <raphael@affinitybridge.com>
2017-04-05 14:30:08 -0700

    add "diff=php" git attribute for Drupal files
    see https://drunkenmonkey.at/blog/diffs_with_proper_function_context

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ln -s ~/.raphaelize/.gitconfig ~/.gitconfig
-ln -s ~/.raphaelize/.gitglobalignore ~/.gitglobalignore
+ln -s ~/.raphaelize/.gitglobalattributes ~/.gitglobalattributes
 
 ln -s ~/.raphaelize/.vim ~/.vim
 ln -s ~/.raphaelize/.vimrc ~/.vimrc
