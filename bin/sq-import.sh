964f5b27e7e6cb79d3b03456d9d87adfac640663
Raphael Huefner <raphaelhuefner@gmail.com>
2026-08-26 16:56:53 +0200

    Improve comments about `sq "forced key mgmt"

--- bin/sq-import.sh
+++ bin/sq-import.sh
@@ -23,7 +23,33 @@ sq pki link authorize --unconstrained --all --cert="${FINGERPRINT}"
 
 # # The sq keystore stores public keys (certs) here:
 # cd ~/Library/Application\ Support/pgp.cert.d
-# To delete public keys, use the `sqlite3` tool with the indexes
+#
+# To "delete" public keys, you can "hack" the index and cache DBs with `sqlite3`:
+#
+# sqlite3 _sequoia_cert_store_index_v1_on_*.sqlite
+#
+# sqlite> .headers on
+# sqlite> .tables
+# sqlite> SELECT * FROM userids;
+# sqlite> SELECT * FROM keys;
+# sqlite> SELECT * FROM certs;
+# sqlite> SELECT * FROM certd_tag;
+# sqlite> DELETE FROM keys WHERE cert_fingerprint = "0123abc...";
+# sqlite> DELETE FROM certs WHERE cert_fingerprint = "0123abc...";
+# sqlite> DELETE FROM userids WHERE cert_fingerprint = "0123abc...";
+# sqlite> DELETE FROM certd_tag WHERE ???;
+# sqlite> .exit;
+#
+#
+# sqlite3 _sequoia_signature_verification_cache_v1_on_*.sqlite
+#
+# sqlite> .headers on
+# sqlite> .tables
+# sqlite> SELECT * FROM entries;
+# sqlite> DELETE FROM entries WHERE ???;
+# sqlite> .exit;
+#
+#
 
-# # Double-check those all those paths with this:
+# # You can double-check all those file system paths with this:
 # sq config inspect paths
